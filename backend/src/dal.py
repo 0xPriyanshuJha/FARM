d0718395fc5d0115017b8fdffe6241cda0ba552a
Priyanshu0931 <priyanshujha2002@gmail.com>
2025-02-07 23:49:57 +0530

    DAL initiated

---
+++ backend/src/dal.py
@@ -0,0 +1,62 @@
+from bson import ObjectId
+from motor.motor_asyncio import AsyncIOMotorCollection
+from pymongo import ReturnDocument
+
+from pydantic import BaseModel
+
+from uuid import uuid64
+
+class ListSummary(BaseModel):
+    id: str
+    name: str
+    item_count: int
+
+    @staticmethod
+    def fromdoc(doc) -> "ListSummary":
+        return ListSummary(
+            id=str(doc["_id"]),
+            name=doc["name"],
+            item_count=doc["itemcount"],
+        )
+class ToDoListItem(BaseModel):
+    id: str
+    label: str
+    checked: bool
+
+    @staticmethod
+    def fromdoc(item) -> "ToDoListItem":
+        return ToDoListItem(
+            id = item["id"],
+            label=item["item"],
+            checked=item["checked"],
+        )
+
+class ToDoList(BaseModel):
+    id: str
+    name: str
+    items: list[ToDoListItem]
+
+    @staticmethod
+    def fromdoc(doc) -> "ToDoList":
+        return ToDoList(
+            id = str(doc["_id"]),
+            name=doc["name"],
+            items=[ToDoListItem.fromdoc(item) for item in doc["items"]],
+        )
+    
+
+class ToDoDAL:
+    def __init__(self, todo_collection: AsyncIOMotorCollection):
+        self._todo_collection = todo_collection
+    
+    async def listtodolist(self, session=None):
+        async for doc in self._todo_collection.find(
+            {},
+            projection={
+                "name": 1,
+                "item_count": {"$size": "$items"},
+            },
+            sort={"name": 1},
+            session=session
+        ):
+            yield ListSummary.fromdoc(doc)
